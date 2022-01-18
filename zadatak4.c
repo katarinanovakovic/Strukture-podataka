81d7b95e195c08d801c1fc0eeb93c00e50c93bd4
katarinanovakovic <92431569+katarinanovakovic@users.noreply.github.com>
2022-01-18 20:17:21 +0100

    Update zadatak4.c

--- zadatak4.c
+++ zadatak4.c
@@ -1,163 +1,331 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 50
 
-typedef struct polinom* pozicija;
-typedef struct polinom{
-    int koef;
-    int eksp;
-    pozicija next;
+#define MAX_LINE 1024
+#define MAX_NAME 20
 
-}Polinom;
+typedef struct polynomialElement* element;
+typedef struct polynomialElement {
+	int coefficient;
+	int exponent;
+	element next;
+};
 
-int umetniElementSortirano(Polinom* head, int koef, int eksp);
-int procitajRed(Polinom *head, char* red);
-int procitajDatoteku(Polinom *head1, Polinom *head2);
-int ispis(Polinom * head);
-int zbrojiPolinome( Polinom* head1, Polinom * head2, Polinom * headZbr);
-int pomnoziPolinome(Polinom * head1, Polinom * head2, Polinom * headUmn);
+element AllocateMemory();
+int ReadFile(element, element);
+int InputElementsIntoTheList(element, char*);
+element input(int, int);
+int SortedInput(element, element);
+element SummationOfPolynomials(element, element);
+element AdditionOfPolynomials(element, element);
+int Print(element);
 
-
-
-int main(){
-    Polinom *head1=(Polinom*)malloc(sizeof(Polinom));
-     Polinom *head2=(Polinom*)malloc(sizeof(Polinom));
-    Polinom *headZbr=(Polinom*)malloc(sizeof(Polinom));
-    Polinom *headUmn=(Polinom*)malloc(sizeof(Polinom));
-
-    procitajDatoteku(head1, head2);
-    printf("Prvi polinom je:\n");
-    ispis(head1);
-    printf("DRugi polinom:\n ");
-    ispis(head2);
-
-    zbrojiPolinome(head1, head2, headZbr);
-    printf("Zbroj polinoma je: \n");
-    ispis(headZbr);
-
-    pomnoziPolinome(head1, head2, headUmn);
-    printf("Umnozak polinoma je. \n");
-    ispis(headUmn);
-
-
-
-
-
-    return 0;
-}
-
-int procitajDatoteku(Polinom *head1, Polinom *head2){
-    FILE* datoteka;
-    
-    int n;
-    char linija[MAX];
-
-    datoteka=fopen("polinomi.txt", "r");
-    if(datoteka==NULL){
-        printf("Neuspjesno otvaranje datoteke");
-    }
-
-
-    fgets(linija, MAX, datoteka);
-    procitajRed(head1,linija);
-    fgets(linija, MAX, datoteka);
-    procitajRed(head2, linija);
-
-
-
-}
-
-int procitajRed(Polinom *head, char* red){
-    int koeficijent = 0;
-    int eksponent = 0;
-    char * pRed = red;
-
-    do {
-        int n = 0;
-
-        sscanf(pRed, " %d %d%n", &koeficijent, &eksponent, &n);
-        pRed += n;
-
-        umetniElementSortirano(head, koeficijent, eksponent);
-    } while (strlen(pRed)>1);
-    
-    return 0;
-
-}
-int umetniElementSortirano(Polinom *head, int koef, int eksp)
+int main()
 {
-    Polinom* P=head;
-    Polinom* novi; 
+	element head1 = AllocateMemory();
+	element head2 = AllocateMemory();
+	element resultOfSummation = AllocateMemory();
+	element resultOfAddition = AllocateMemory();
 
+	ReadFile(head1, head2);
 
-    while(P->next!=NULL && (P->next->eksp) > eksp)
-        P=P->next;
+	printf("First polynomial:\n");
+	Print(head1->next);
+	printf("Second polynomial:\n");
+	Print(head2->next);
 
-    if (P->next != NULL && P->next->eksp == eksp) {
-            P->next->koef+=koef;
-    }
-    else {
-         
-        novi=(Polinom *)malloc(sizeof(Polinom));
+	resultOfSummation = SummationOfPolynomials(head1, head2);
 
-        if (!novi)
-            return -1;
+	printf("Result of summation:\n");
+	Print(resultOfSummation->next);
 
-        novi->next=P->next;
-        P->next=novi;
+	resultOfAddition = AdditionOfPolynomials(head1, head2);
 
-        novi->koef=koef;
-        novi->eksp=eksp;
-    }
-
-    return 0;
-}
-
-int ispis(Polinom * head)
-{
-    Polinom * P = head->next;
-
-    while(P!=NULL) {
-        printf("%dx^%d", P->koef, P->eksp);
-        if (P->next != NULL)
-        
-        P=P->next;
-    }
+	printf("Result of addition:\n");
+	Print(resultOfAddition->next->next);
 
 	return 0;
 }
 
-int zbrojiPolinome( Polinom* head1, Polinom * head2, Polinom * headZbr)
+element AllocateMemory()
 {
-    Polinom * Phead1 = head1->next;
-    Polinom * Phead2 = head2->next;
+	element newElement = NULL;
 
-    while(Phead1 != NULL) {
-        umetniElementSortirano(headZbr, Phead1->koef, Phead1->eksp);
-        Phead1 = Phead1->next;
-    }
+	newElement = (element)malloc(sizeof(struct polynomialElement));
 
-    while(Phead2 != NULL) {
-        umetniElementSortirano(headZbr, Phead2->koef, Phead2->eksp);
-        Phead2 = Phead2->next;
-    }
+	if (!newElement)
+	{
+		printf("Cannot allocate memory!\n");
+		return -1;
+	}
 
-    return 0;
+	newElement->coefficient = 0;
+	newElement->exponent = 0;
+	newElement->next = NULL;
+
+	return newElement;
 }
 
-int pomnoziPolinome(Polinom * head1, Polinom * head2, Polinom * headUmn)
+int ReadFile(element head1, element head2)
 {
-     Polinom *Phead1 = head1->next;
-     Polinom *Phead2 = head2->next;
-     
-     while(Phead1 != NULL) {
-         Phead2=head2->next;
-         while(Phead2 != NULL) {
-             umetniElementSortirano(headUmn, Phead1->koef*Phead2->koef, Phead1->eksp+Phead2->eksp);
-             Phead2=Phead2->next;
-         }
-         Phead1=Phead1->next;
-     }
+	char buffer[MAX_LINE] = { 0 };
+	char fileName[MAX_NAME] = { 0 };
 
-     return EXIT_SUCCESS;}
+	printf("Enter the name of the file:");
+	scanf(" %s", fileName);
+
+	FILE* file = fopen(fileName, "r");
+
+	if (!file)
+	{
+		printf("Cannot access the file!\n");
+		return -2;
+	}
+
+	fgets(buffer, MAX_LINE, file);
+	InputElementsIntoTheList(head1, buffer);
+
+	fgets(buffer, MAX_LINE, file);
+	InputElementsIntoTheList(head2, buffer);
+
+	fclose(file);
+
+	return 0;
+}
+
+int InputElementsIntoTheList(element head, char* buffer)
+{
+	char* temp = buffer;
+	int coefficient = 0;
+	int exponent = 0;
+	int numbytes = 0;
+	int status = 0;
+	element newElement = NULL;
+
+	while (strlen(temp) > 2)
+	{
+		status = sscanf(temp, " %d %d %n", &coefficient, &exponent, &numbytes);
+
+		if (status != 2)
+		{
+			printf("Error in the file!\n");
+			return -3;
+		}
+
+		newElement = input(coefficient, exponent);
+
+		if (!newElement)
+		{
+			printf("Error in creating new element");
+			return -4;
+		}
+
+		SortedInput(head, newElement);
+
+		temp += numbytes;
+	}
+
+	return 0;
+}
+
+element input(int coefficient, int exponent)
+{
+	element newElement = AllocateMemory();
+
+	newElement->coefficient = coefficient;
+	newElement->exponent = exponent;
+
+	return newElement;
+}
+
+int SortedInput(element head, element newElement)
+{
+	element temp = head;
+	int result = 0;
+	element deletedElement = NULL;
+
+	while (temp->next != NULL && temp->next->exponent < newElement->exponent)
+	{
+		temp = temp->next;
+	}
+
+	if (temp->next == NULL || temp->next->exponent != newElement->exponent)
+	{
+		newElement->next = temp->next;
+		temp->next = newElement;
+	}
+	else
+	{
+		result = temp->next->coefficient + newElement->coefficient;
+
+		if (result == 0)
+		{
+			deletedElement = temp->next;
+			temp->next = deletedElement->next;
+			free(deletedElement);
+		}
+		else
+		{
+			temp->next->coefficient = result;
+		}
+
+		free(newElement);
+	}
+
+	return 0;
+}
+
+element SummationOfPolynomials(element head1, element head2)
+{
+	element temp1 = head1;
+	element temp2 = head2;
+	element result = AllocateMemory();
+	element newElement = NULL;
+
+	while (temp1 != NULL)
+	{
+		newElement = input(temp1->coefficient, temp1->exponent);
+
+		if (!newElement)
+		{
+			printf("Error in creating newElement");
+			return -4;
+		}
+
+		SortedInput(result, newElement);
+
+		temp1 = temp1->next;
+	}
+
+	while (temp2 != NULL)
+	{
+		newElement = input(temp2->coefficient, temp2->exponent);
+
+		if (!newElement)
+		{
+			printf("Error in creating new element!\n");
+			return -4;
+		}
+
+		SortedInput(result, newElement);
+
+		temp2 = temp2->next;
+	}
+
+	return result;
+}
+
+element AdditionOfPolynomials(element head1, element head2)
+{
+	element temp1 = head1;
+	element temp2 = head2;
+	element result = AllocateMemory();
+	element newElement = NULL;
+
+	while (temp1 != NULL)
+	{
+		while (temp2 != NULL)
+		{
+			newElement = input(temp1->coefficient * temp2->coefficient, temp1->exponent + temp2->exponent);
+
+			if (!newElement)
+			{
+				printf("Error in creating new element!\n");
+				return -4;
+			}
+
+			SortedInput(result, newElement);
+
+			temp2 = temp2->next;
+		}
+
+		temp2 = head2;
+		temp1 = temp1->next;
+	}
+
+	return result;
+}
+
+int Print(element head)
+{
+	if (head->coefficient < 0)
+	{
+		if (head->exponent == 1)
+		{
+			printf("- %dx ", abs(head->coefficient));
+		}
+		else
+		{
+			printf("- %dx^%d ", abs(head->coefficient), head->exponent);
+		}
+	}
+	else if (head->coefficient == 1)
+	{
+		if (head->exponent == 1)
+		{
+			printf("x ");
+		}
+		else
+		{
+			printf("x^%d ", head->exponent);
+		}
+	}
+	else
+	{
+		if (head->exponent == 1)
+		{
+			printf("%dx ", head->coefficient);
+		}
+		else
+		{
+			printf("%dx^%d ", head->coefficient, head->exponent);
+		}
+	}
+
+	head = head->next;
+
+	while (head)
+	{
+		if (head->coefficient < 0)
+		{
+			if (head->exponent == 1)
+			{
+				printf("- %dx ", abs(head->coefficient));
+			}
+			else
+			{
+				printf("- %dx^%d ", abs(head->coefficient), head->exponent);
+			}
+		}
+		else if (head->coefficient == 1)
+		{
+			if (head->exponent == 1)
+			{
+				printf("+ x ");
+			}
+			else
+			{
+				printf(" + x^%d ", head->exponent);
+			}
+		}
+		else
+		{
+			if (head->exponent == 1)
+			{
+				printf("+ %dx ", head->coefficient);
+			}
+			else
+			{
+				printf("+ %dx^%d ", head->coefficient, head->exponent);
+			}
+		}
+
+		head = head->next;
+	}
+
+	printf("\n");
+
+	return 0;
+}
